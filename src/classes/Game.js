15c3c1b77cc56a67a57defec1809590b87e50054
Kaiser <dev.khagonia@gmail.com>
2023-02-12 22:10:49 +0800

    Bug Fix Update - Game Logic
    Updated a bug where the game ends even if there are no four consecutively connected chips.
    
    This is due to an error in the game logic. This has been fixed.

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -32,58 +32,72 @@ class ConnectFour {
     let connectedDownLeft  = 1;
     let connectedDownRight = 1;
     let connectedUpRight   = 1;
-    let tempDownLeft = 1;
     const center = this.#cols*col + row;
 
-    // check down, left and right
-    for(let i = 1; i < 5; i++) {
+    // check down
+    for(let i = 1; i < 4; i++) {
       const indexBelow = center + i;
+
+      if(indexBelow >= this.#board.length || this.#board[indexBelow] !== this.#board[center]) break;
+      connectedBelow++;
+    }
+
+    // check left
+    for(let i = 1; i < 4; i++) {
       const indexLeft = center - (this.#cols*i);
+
+      if(indexLeft < 0 || this.#board[indexLeft] !== this.#board[center]) break;
+      connectedLeft++;
+    }
+
+    // check right
+    for(let i = 1; i < 4; i++) {
       const indexRight = center + (this.#cols*i);
 
-      if (this.#board[indexBelow] === this.#board[center]) connectedBelow++;
-      if(indexLeft > 0 && this.#board[indexLeft] === this.#board[center]) connectedLeft++;
-      if(indexRight < this.#board.length && this.#board[indexRight] === this.#board[center]) connectedRight++;
+      if(indexRight > this.#board.length || this.#board[indexRight] !== this.#board[center]) break;
+      connectedRight++;
     }
 
     //check upper left
-    for(let i = 1; i < 5; i++) {
+    for(let i = 1; i < 4; i++) {
       const indexUpLeft = center - (this.#cols*i) - i;
 
       if(indexUpLeft%this.#cols === 6 || indexUpLeft < 0) break;
-      if(this.#board[indexUpLeft] === this.#board[center]) connectedUpLeft++;
+      if(this.#board[indexUpLeft] !== this.#board[center]) break;
+      connectedUpLeft++;
     }
 
     //check down left
-    for(let i = 1; i < 5; i++) {
+    for(let i = 1; i < 4; i++) {
       const indexDownLeft = center - (this.#cols*i) + i;
 
       if(indexDownLeft%this.#cols === 0 || indexDownLeft < 0) break;
-      if(this.#board[indexDownLeft] === this.#board[center]) connectedDownLeft++;
+      if(this.#board[indexDownLeft] !== this.#board[center]) break;
+      connectedDownLeft++;
     }
 
     //check down right
-    for(let i = 1; i < 5; i++) {
+    for(let i = 1; i < 4; i++) {
       const indexDownRight = center + (this.#cols*i) + i;
 
       if(indexDownRight%this.#cols === 0 || indexDownRight >= this.#board.length) break;
-      if(this.#board[indexDownRight === this.#board[center]]) connectedDownRight++;
+      if(this.#board[indexDownRight] !== this.#board[center]) break;
+      connectedDownRight++;
     }
 
     //check up right 
-
-    for(let i = 1; i < 5; i++) {
+    for(let i = 1; i < 4; i++) {
       const indexUpRight = center + (this.#cols*i) - i;
 
       if(indexUpRight%this.#cols === 6 || indexUpRight >= this.#board.length) break;
-      if(this.#board[indexUpRight] === this.#board[center]) connectedUpRight++;
+      if(this.#board[indexUpRight] !== this.#board[center]) break;
+      connectedUpRight++;
     }
 
-
-    if (connectedBelow === 5) return true;
-    if (connectedLeft + connectedRight - 1 === 5) return true;
-    if (connectedUpLeft + connectedDownRight - 1 === 5) return true;
-    if (connectedUpRight + connectedDownLeft - 1 === 5) return true;
+    if (connectedBelow >= 4) return true;
+    if (connectedLeft + connectedRight - 1 >= 4) return true;
+    if (connectedUpLeft + connectedDownRight - 1 >= 4) return true;
+    if (connectedUpRight + connectedDownLeft - 1 >= 4) return true;
     return false;
   }
 
